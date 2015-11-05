6c859f278cc22196e8d474d1106bb29f0e3a100c
swiftyy188 <jeffho.ml@gmail.com>
2015-11-05 15:46:04 -0500

    first commit

---
+++ CRM.rb
@@ -0,0 +1,20 @@
+require_relative 'contact'
+require 'sinatra'
+
+get "/" do
+  @crm_app_name = "My CRM"
+  erb :index
+end
+
+get "/contacts" do
+  erb :contacts
+end
+
+get "/contacts/new" do
+  erb :new_contact
+end
+
+post "/contacts" do
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  redirect to('/contacts')
+end
